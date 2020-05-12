b9164d207a113c16be57a852f6a9efcf7b9f58e6
huynet <cocwedc@gmail.com>
2020-05-12 15:36:24 -0500

    exsgo

---
+++ lib/mixenticon.ex
@@ -0,0 +1,18 @@
+defmodule Mixenticon do
+  @moduledoc """
+  Documentation for `Mixenticon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mixenticon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mixenticon_test.exs
@@ -0,0 +1,8 @@
+defmodule MixenticonTest do
+  use ExUnit.Case
+  doctest Mixenticon
+
+  test "greets the world" do
+    assert Mixenticon.hello() == :world
+  end
+end
